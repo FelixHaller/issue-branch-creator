c6985e86bee4f56d0851e5d1e82afe360bfe3b21
Patrick Bachmann <patb@spreadshirt.net>
2021-03-23 15:04:45 +0100

    remove version from properties

--- create-release.py
+++ create-release.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import re
 import sys
 from datetime import datetime
 from subprocess import run, CalledProcessError
@@ -9,21 +8,10 @@ PROPERTIES_FILENAME = "gradle.properties"
 
 def main():
     tag_name = datetime.utcnow().strftime("%Y.%m.%d.%H%M")
-    update_plugin_version(tag_name)
     create_tag(tag_name)
     push_tags(tag_name)
 
     print(f"sucessfully pushed release tag {tag_name}")
-
-
-def update_plugin_version(version):
-    with open(PROPERTIES_FILENAME, "r") as config:
-        lines = config.readlines()
-    for i, line in enumerate(lines):
-        if re.match("pluginVersion\\s*=", line):
-            lines[i] = f"pluginVersion={version}\n"
-    with open(PROPERTIES_FILENAME, "w") as config:
-        config.writelines(lines)
 
 
 def create_tag(tag_name):
